6c799001ed369cb93ffacb283608241f82c762de
Kyoung Hoon Kim <bucketkim@kyoungucbookpro.myfiosgateway.com>
2023-04-25 21:00:40 -0400

    procfile change

--- server.js
+++ server.js
@@ -1,32 +1,3 @@
-// const bodyParser = require("body-parser");
-// const MongoClient = require("mongodb").MongoClient;
-// const newRouter = require("./router.js");
-
-// const express = require("express");
-// const app = express();
-
-// const cors = require("cors");
-// app.use(cors());
-
-// app.use(bodyParser.json());
-
-// MongoClient.connect("mongodb://localhost:27017") // This is the location of where your local database is living.
-//   .then((client) => {
-//     const db = client.db("email_db"); // The name we gave our DB
-//     const staffCollection = db.collection("email"); // The name we gave our collection inside the DB
-//     const emailRouter = newRouter(staffCollection); // We haven't built the router functionality yet, but we will next!
-
-//     app.use("/api/email", emailRouter); // Defining the base route where we can later access our data
-//   })
-//   .catch(console.err);
-
-// app.listen(4000, function () {
-//   console.log(`Listening on this port: ${this.address().port}`);
-// });
-
-// mongodb+srv://bkim07:<password>@cluster0.pkfjf7i.mongodb.net/test
-// mongodb+srv://bkim07:<password>@cluster0.pkfjf7i.mongodb.net/test
-
 require("dotenv").config();
 const express = require("express");
 const app = express();
